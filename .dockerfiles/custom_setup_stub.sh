ba206edc78e0bfeb577ed53e56dab568b064f9a0
Clemens Rabe <clemens.rabe@clemensrabe.de>
2023-11-15 12:05:49 +0100

    bugfix: Ensure at least one custom_setup_*.sh file is present. Otherwise docker can't build the docker images.

---
+++ .dockerfiles/custom_setup_stub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# This stub is only there to prevent errors when not using the
+# MAKE4PY_DOCKER_SETUP_SCRIPTS variable of .make4py. It satisfies
+# the COPY custom_setup_*.sh /tmp command of the Dockerfile.
